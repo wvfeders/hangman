99bd3fb82fe4732eae9eb040c3d4988e836b049d
wvfeders <wvfedders@frontier.com>
2017-12-15 16:36:21 -0500

    More work

--- hangman.rb
+++ hangman.rb
@@ -4,8 +4,8 @@ word = gets.chomp
 
 word_letters = word.split("")
 letter_count = word_letters.count
-
-
+correct_letters = []
+incorrect_letters = []
 
 word_guess = ["-"] * letter_count
 
@@ -18,8 +18,11 @@ count=0
 word_guess.each do |i|
 	if letter == word_letters[count]
 		word_guess[count] = letter
+		correct_letters.push(letter)
+	elsif 
+		incorrect_letters.push(letter)
 	end
 count = count +1
 end
 
-print word_guess
+print word_guess, correct_letters, incorrect_letters
